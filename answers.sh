84226b030a8da52c55c9ee1f85e4749079f83815
zeinab <zeinabhteit1@gmail.com>
2024-12-10 20:00:44 +0200

    Basic Queries

--- answers.sh
+++ answers.sh
@@ -7,6 +7,25 @@ INSERT INTO students ( name, age, gender, points) VALUES ('Zeinab', 21, 'f', 200
 UPDATE students SET points = points + 10 WHERE name ='Basma';
 UPDATE students SET points = points - 5 WHERE name='Alex';
 
+CREATE TABLE graduates (
+ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+Name TEXT NOT NULL UNIQUE,
+Age INTEGER,
+Gender TEXT,
+Points INTEGER,
+Graduation TEXT
+);
+CREATE TABLE graduates (
+ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+Name TEXT NOT NULL UNIQUE,
+Age INTEGER,
+Gender TEXT,
+Points INTEGER,
+Graduation TEXT
+);
+UPDATE graduates
+SET Graduation ='08/09/2018'
+WHERE name ='Layal';
 
 
 
